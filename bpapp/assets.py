437cfef30427253c4f36117a070179221bbb5532
Alexandre Carlos <alexandre.carlos@gmail.com>
2016-02-09 21:58:03 +0000

    date time picker v2

--- bpapp/assets.py
+++ bpapp/assets.py
@@ -4,9 +4,9 @@ from flask_assets import Bundle, Environment
 
 css = Bundle(
     'libs/bootstrap/dist/css/bootstrap.css',
-    'css/style.css',
-    # 'css/ct-paper.css',
-    # 'css/bootstrap-datetimepicker.min.css',
+    # 'css/style.css',
+    'css/bootstrap-datetimepicker.css',
+
     filters='cssmin',
     output='public/css/common.css'
 )
@@ -14,14 +14,9 @@ css = Bundle(
 js = Bundle(
     'libs/jQuery/dist/jquery.js',
     'libs/bootstrap/dist/js/bootstrap.js',
-    'js/plugins.js',
-    'js/moment-with-locales.min.js',
-    # 'js/bootstrap-datetimepicker.min.js',
-    # 'js/bootstrap-datepicker.js',
-    # 'js/bootstrap-select.js',
-    # 'js/ct-paper.js',
-    # 'js/ct-paper-checkbox.js',
-    # 'js/ct-paper-radio.js',
+    'js/script.js',
+    'js/bootstrap-datetimepicker.js',
+    'js/locales/bootstrap-datetimepicker.pt.js',
     filters='jsmin',
     output='public/js/common.js'
 )
